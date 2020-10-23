48072305b489d1c9fa887050237cc5e4c855ebe8
samira khadem <samira.khademhoseini@gmail.com>
2020-10-23 14:06:55 +0200

    first commit

---
+++ src/Samira/Main.java
@@ -0,0 +1,20 @@
+package Samira;
+
+public class Main {
+
+    static double calculateRectArea( double height, double width) {
+        return height * width;
+
+        //Scanner input = new Scanner(System.in);
+        //System.out.println("Enter the Side: ");
+        //double side = input.nextDouble();
+        //double area = side * side;
+        //System.out.println(area);
+
+}
+
+    public static void main(String[] args) {
+        calculateRectArea(20, 30);
+        calculateRectArea(40,50);
+    }
+}
---
+++ src/Samira/MainRect1.java
@@ -0,0 +1,18 @@
+package Samira;
+
+public class MainRect1 {
+
+
+    static void calculate() {
+
+        double height = 2;
+        double width = 5;
+        System.out.println(height*width);
+
+    }
+
+    public static void main(String[] args) {
+        calculate();
+    }
+}
+
